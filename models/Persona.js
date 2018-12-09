e41356039799ba2103a8bee7448562c58c08fda0
Edisson Reinozo <edisson.reinozo@ucuenca.edu.ec>
2018-12-08 22:28:24 -0500

    Model

---
+++ models/Persona.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, type) => {
+  const Persona = sequelize.define('persona', {
+    nombres: {
+      type: type.STRING
+    },
+    apellidos: {
+      type: type.STRING
+    },
+    telefono: {
+      type: type.STRING
+    }
+  }, {
+    timestamps: true
+  })
+  return Persona
+}
